193a0d92c1d7a4e8d78f8efc53a1cc36643366d7
Roman505050 <myhun59@gmail.com>
2024-04-02 11:39:47 +0300

    Add Ownable contract and modify Bank contract

--- Bank.sol
+++ Bank.sol
@@ -1,20 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Bank {
+contract Ownable {
+    address public ownerContract;
+
+    constructor() {
+        ownerContract = msg.sender;
+    }
+
+    modifier testOwner() {
+        require(msg.sender == ownerContract, "Only the contract owner can call this function");
+        _;
+    }
+}
+
+contract Bank is Ownable {
     address public owner;
     mapping (address => uint) public  payments;
+    event LogMessage(address sender);
+
+    function printMessage(address sender) public {
+        emit LogMessage(sender);
+    }
 
     constructor() {
         owner = msg.sender;
+        printMessage(msg.sender);
     }
 
     function deposit() public payable {
         payments[msg.sender] += msg.value;
     }
 
-    function withdrawAll() public {
+    function withdraw() public {
         address payable _to = payable(msg.sender);
+        uint amount = payments[msg.sender];
+        if (amount != 0) {
+            _to.transfer(amount);   
+        } else {
+            revert("Your balance is zero");
+        }
+        
+    }
+
+    function withdrawAll() public testOwner {
+        address payable _to = payable (msg.sender);
         address _thisContract = address(this);
         _to.transfer(_thisContract.balance);
     }
